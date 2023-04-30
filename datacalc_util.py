f578c99c7c069162dff03eec6ac343b6253f7b03
bespin PC <limhyosuk@yahoo.com>
2023-04-30 18:54:31 +0900

    20300430 modifty

--- datacalc_util.py
+++ datacalc_util.py
@@ -1,12 +1,14 @@
 from datetime import date, datetime, time, timedelta
 
+
 def get_vs_ta_ym_mnh_std(vd_ced):
-  # 2월을 포함하는 다음 달 1일 계산
-  if vd_ced.month == 12:
-    next_month = datetime(vd_ced.year + 1, 1, 1)
-  else:
-    next_month = datetime(vd_ced.year, vd_ced.month + 1, 1)
-  return next_month
+    # 2월을 포함하는 다음 달 1일 계산
+    if vd_ced.month == 12:
+        next_month = datetime(vd_ced.year + 1, 1, 1)
+    else:
+        next_month = datetime(vd_ced.year, vd_ced.month + 1, 1)
+    return next_month
+
 
 vs_today = date.today().strftime("%Y%m%d")
 # vs_ced = datetime.strptime(vs_today, "%Y%m%d").date()
@@ -23,7 +25,4 @@ vs_ta_ym_etd = (datetime.strptime(str(vs_ced)[0:6], "%Y%m"))
 
 vs_ta_ym_mnh_std = get_vs_ta_ym_mnh_std(vd_ced).strftime("%Y%m%d")
 
-print(vs_ta_ym_mnh_std)
-
-
-
+print("vs_ta_ym_mnh_std : ", vs_ta_ym_mnh_std)
